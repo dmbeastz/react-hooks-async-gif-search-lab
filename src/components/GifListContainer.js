bcfaf88a78c053ff1e0ab3656466675e98a07f23
dmbeastz <daveroymutisya2@gmail.com>
2023-11-26 00:31:50 +0300

    Done

---
+++ src/components/GifListContainer.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import GifList from './GifList';
+import GifSearch from './GifSearch';
+
+const GifListContainer = () => {
+  const [gifs, setGifs] = useState([]);
+  
+  const fetchGifs = (query) => {
+   
+  
+    const apiKey = 'dc6zaTOxFJmzC';
+    const apiUrl = `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${apiKey}&rating=g`;
+    
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => setGifs(data.data.slice(0, 3))) 
+      .catch(error => console.error('Error fetching data:', error));
+  };
+
+  return (
+    <div>
+      <GifSearch onSearch={fetchGifs} />
+      <GifList gifs={gifs} />
+    </div>
+  );
+}
+
+export default GifListContainer;
